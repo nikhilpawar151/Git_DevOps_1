4ac309cf73db8def8787288076e389aaa2a79e40
= <nikhilpawar151@gmail.com>
2022-07-01 20:44:10 +0530

    Changes in file 3 & 4

--- wel3.java
+++ wel3.java
@@ -1,5 +1,6 @@
 class wel3{
 	public static void main(String a[]){
 		System.out.println("Welcome to India");
+		System.out.println("Stash changes in wel3.java file");
 	}
 }
--- wel4.java
+++ wel4.java
@@ -1,5 +1,6 @@
 class wel4{
 	public static void main String(String a[]){
-		System.out.println("wel4 file in welcome_ext1 branch");
+		System.out.println("wel4 file in welcome_ext1 branch.");
+		System.out.println("Added stash changes in file 4");
 	}
 }
